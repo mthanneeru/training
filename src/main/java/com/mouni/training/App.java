dfe3b5cb99dae67315f8c85c07b9056fc3a20f91
Mounica <mounica.thanneeru@gmail.com>
2015-03-20 14:31:04 -0700

    Initial Checkin

---
+++ src/main/java/com/mouni/training/App.java
@@ -0,0 +1,16 @@
+package com.mouni.training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello America!" );
+        System.out.println( "Hello India!" );
+        System.out.println( "Hello California!" );
+    }
+}
